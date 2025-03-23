6b5b5cd123488c11a30cf56e59f73c2284a24c16
logey1314 <logey1314@gmail.com>
2025-03-23 16:34:19 +0800

    更新

---
+++ day11/src/com/Demo/demo5/Test.java
@@ -0,0 +1,7 @@
+package com.Demo.demo5;
+
+public class Test {
+    public static void main(String[] args) {
+        // 请在这里编写代码
+    }
+}
--- day11/src/com/home/work2/Test.java
+++ day11/src/com/home/work2/Test.java
@@ -1,15 +1,28 @@
 package com.home.work2;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Test  {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         List<String> list = new ArrayList<>();
         list.add("迪丽热巴");
         list.add("古力娜扎");
         list.add("周杰伦");
         list.add("蔡徐坤");
+
+
+        FileWriter witer=new FileWriter("day11/file/2.txt");
+
+        for (String s : list) {
+            witer.write(s);
+            witer.write("\n");
+        }
+        witer.close();
 
     }
 }
